cec35e08b376c70ff88ed75d4b058e2cf476ea8b
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-01-11 23:46:14 +0200

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #a manifest file to create a file named school in /tmp path
-file { '/tmp/school':
-	mode => '0744'
-	owner => 'www-data'
-	group => 'www-data'
-	content => 'I love Puppet'
 
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
