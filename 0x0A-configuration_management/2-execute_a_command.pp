02db4d9abfb3b45efcd5f9147551b1622c75711b
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-01-11 23:52:00 +0200

    configuration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #a manifest that executes a command
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
