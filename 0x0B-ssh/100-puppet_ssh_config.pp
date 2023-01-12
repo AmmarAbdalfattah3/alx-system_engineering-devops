e72b4b2ecd367c395a790eb7489b595256eb562a
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-01-12 23:49:05 +0200

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#make changes to our configuration file using puppet
+file_line { 'identify file':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	replace => 'true'
+	line => '	IdentifyFile ~/.ssh/school'
+}
+
+file_line { 'prevent password authentication':
+	ensure => 'present',
+	path => '/etc/ssh/ssh_config',
+	replace => 'true'
+	line => '	PasswordAuthentication no'
+}
