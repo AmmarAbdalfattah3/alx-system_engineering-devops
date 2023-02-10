51646145dba47a9c2b6e1926979498d061ce0f49
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-02-10 14:01:52 -0800

    API tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ api = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
     employ_id = sys.argv[1]
-    file_name = '{}.csv'.format(employ)
+    file_name = '{}.csv'.format(employ_id)
     employ_info = requests.get('{}/users/{}'.format(api, employ_id))
     employ_info = employ_info.json()
     todos = requests.get('{}/todos?userId={}'.format(api, employ_id))
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     with open(file_name, mode="w") as csv_file:
         export = csv.writer(csv_file, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            export.writerow((todo['userId'], user_info['username'],
-                             todo['completed'], todo['title']))
+        for row in todos:
+            export.writerow((row['userId'], user_info['username'],
+                             row['completed'], row['title']))
