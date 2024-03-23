5507541b14623e375025efc75adbf0f5b2ccb2ee
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-03-22 17:22:49 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#a manifest file to create a file named school in `/tmp` path
+
+file {'/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data'
+}
