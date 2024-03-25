21294b62a4cdfbcf3dc4d6e30d51816631bcb4d2
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-03-24 19:39:11 -0700

    Client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file_line { 'identify file':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => 'true',
+  line    => '	IdentifyFile ~/.ssh/school'
+}
+
+file_line { 'prevent password authentication':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => 'true',
+  line    => '	PasswordAuthentication no',
+}
