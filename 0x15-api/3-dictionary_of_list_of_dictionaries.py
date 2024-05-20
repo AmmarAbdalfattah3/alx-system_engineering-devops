48e15d8bff1be2f95346099da034893770c45358
AmmarAbdalfattah3 <ammarabdalfattah@gmail.com>
2024-05-20 16:42:22 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,22 +9,22 @@ import requests
 import sys
 
 api = 'https://jsonplaceholder.typicode.com'
-
 if __name__ == '__main__':
     file_name = 'todo_all_employees.json'
-    employ_info = requests.get('{}/users/'.format(api))
-    employ_info = employ_info.json()
-    all_tasks = {}
-    for user in employ_info:
-        employ_id = user['id']
-        todos = requests.get('{}/todos?userId={}'.format(api, employ_id))
+    users = requests.get('{}/users/'.format(api))
+    users = users.json()
+    emp_todos = {}
+    for usr in users:
+        emp_id = usr['id']
+        todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
         todos = todos.json()
-        user_todos = []
-        for task in todos:
-            tasks = {'username': employ_info.get('username'),
-                     'task': task.get('title'),
-                     'completed': task.get('completed')}
-            user_todos.append(tasks)
-        all_tasks[str(employ_id)] = user_todos
-    with open(file_name, mode="w") as json_file:
-        json.dump(all_tasks, json_file)
+        todos_copy = []
+        for todo in todos:
+            task = {'username': usr['userame'],
+                    'task': todo['title'],
+                    'completed': todo['completed']
+                    }
+            todos_copy.append(task)
+        emp_todos[str(emp_id)] = todos_copy
+    with open(file_name, mode='w') as json_file:
+        json.dump(emp_todos, json_file)
