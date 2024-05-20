1d9a17a0f0d3ba6f07017753dbce74d4652d3f0e
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-05-20 23:14:03 +0200

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""display on the standard output the employee TODO list
+   progress in this exact format
+"""
+
+
+import requests
+import sys
+
+api = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    employ_id = sys.argv[1]
+    employ_info = requests.get('{}/users/{}'.format(api, employ_id))
+    employ_info = employ_info.json()
+    todos = requests.get('{}/todos?userId={}'.format(api, employ_id))
+    todos = todos.json()
+    comp_todos = []
+    for item in todos:
+        if item['completed'] is True:
+            comp_todos.append(item)
+    print("Employee {} is done with tasks({}/{}):".format(employ_info['name'],
+          len(comp_todos), len(todos)))
+    for item in comp_todos:
+        print("\t {}".format(item['title']))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+   Export data in the CSV format
+"""
+
+
+import csv
+import requests
+import sys
+
+api = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    employ_id = sys.argv[1]
+    file_name = '{}.csv'.format(employ_id)
+    employ_info = requests.get('{}/users/{}'.format(api, employ_id))
+    employ_info = employ_info.json()
+    todos = requests.get('{}/todos?userId={}'.format(api, employ_id))
+    todos = todos.json()
+    with open(file_name, mode="w") as csv_file:
+        export = csv.writer(csv_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in todos:
+            export.writerow((row['userId'], employ_info['username'],
+                             row['completed'], row['title']))
