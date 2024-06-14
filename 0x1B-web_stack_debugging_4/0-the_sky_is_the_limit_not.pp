12a462b5d753cd1286f558ae4e1ee22e2379a1b4
AmmarAbdalfattah3 <ammarabdalfattah@gmail.com>
2024-06-13 17:53:29 -0700

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-exec { 'update_nginx_limits':
-  command     => 'sed -i "s/auto/15000/" /etc/default/nginx && systemctl restart nginx',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  logoutput   => true,
-  unless      => 'grep -q "auto" /etc/default/nginx',
+# update limit for nginx
+exec { 'update soft limit for nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# fix hard limit for the user
+exec { 'fix hard limit for the user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# fix soft limit for the user
+exec { 'fix soft limit for ther user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
